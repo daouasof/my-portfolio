fcb31017431dcb5e283bee1b2b1d0de350314493
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-18 14:51:23 -0400

    added About me section

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,12 @@
-import '../App.css';
+import '../styles/App.css';
 import Header from './Header';
+import Main from './Main';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Main />
     </div>
   );
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,23 @@
+import '../styles/Main.css';
+
+function Main() {
+  return (
+    <div>
+      <h2 className="title" id="About">
+        About me
+      </h2>
+      <p>
+        I am passionate about digital projects with a social impact. For several
+        years, I have worked as a Product Owner on softwares aimed at supporting
+        NGOs. I then decided to become a web developer and embarked on an
+        intensive web development bootcamp. My technology skillset includes Ruby
+        on Rails, Javascript, React.JS, HTML and CSS.
+      </p>
+      <h2 className="title" id="Projects">
+        Projects
+      </h2>
+    </div>
+  );
+}
+
+export default Main;

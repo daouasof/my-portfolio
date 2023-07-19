5a86beac3da01cbe4f0359649562448401f7b410
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-19 15:16:43 -0400

    started to work on transitions for navigation

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,16 +2,28 @@ import '../styles/Nav.css';
 import { Link } from 'react-scroll';
 
 function Nav() {
-  const options = ['About', 'Projects', 'Contact'];
+  const options = ['Projects', 'Contact'];
   return (
     <ul className="nav">
+      <li key={`about00`}>
+        <Link
+          to="About"
+          spy={true}
+          smooth={true}
+          offset={-120}
+          duration={500}
+          activeClass="active"
+        >
+          About me
+        </Link>
+      </li>
       {options.map((option, index) => (
         <li key={`${option}${index}`}>
           <Link
             to={`${option}`}
             spy={true}
             smooth={true}
-            offset={-50}
+            offset={-100}
             duration={500}
           >
             {option}

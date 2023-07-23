225afcaad6cf3ab4ed2563e257b4f15d6e064126
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-23 10:08:44 -0300

    put back the header sticky position for large devices

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,10 +6,8 @@ import Social from './Social';
 function Header() {
   return (
     <div className="header-container">
-      <div className="header-sub-container">
-        <Presentation />
-        <Nav />
-      </div>
+      <Presentation />
+      <Nav />
       <Social />
     </div>
   );

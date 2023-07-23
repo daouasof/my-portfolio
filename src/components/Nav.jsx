0d8a6c80817742fe0925ec55bb81cfe10af57c20
Sofia Daouadji <sofia.daouadji@protonmail.com>
2023-07-23 10:02:46 -0300

    ajusted nav behaviour on small devices

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,33 +4,35 @@ import { Link } from 'react-scroll';
 function Nav() {
   const options = ['Projects', 'Contact'];
   return (
-    <ul className="nav">
-      <li key={`about00`}>
-        <Link
-          to="About"
-          spy={true}
-          smooth={true}
-          offset={-120}
-          duration={500}
-          activeClass="active"
-        >
-          About
-        </Link>
-      </li>
-      {options.map((option, index) => (
-        <li key={`${option}${index}`}>
+    <div className="nav-container">
+      <ul className="nav">
+        <li key={`about00`}>
           <Link
-            to={`${option}`}
+            to="About"
             spy={true}
             smooth={true}
-            offset={-100}
+            offset={-120}
             duration={500}
+            activeClass="active"
           >
-            {option}
+            About
           </Link>
         </li>
-      ))}
-    </ul>
+        {options.map((option, index) => (
+          <li key={`${option}${index}`}>
+            <Link
+              to={`${option}`}
+              spy={true}
+              smooth={true}
+              offset={-100}
+              duration={500}
+            >
+              {option}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
